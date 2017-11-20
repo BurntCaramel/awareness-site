d96bf4643674141b97edb7512e81b7174b0db693
Patrick Smith <patrick@burntcaramel.com>
2017-11-21 00:53:15 +1100

    Embed Preact Counter example

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -12,7 +12,37 @@ export default class Home extends Component {
 
 				<p>Works with <strong>React</strong>, <strong>Preact</strong> & <strong>Redux</strong>.</p>
 
-				<p>Less than 2 KB gzipped.</p>
+				<p>Core is <a href="https://bundlephobia.com/result?p=awareness">less than 1 KB gzipped</a>.</p>
+
+				<div>
+					<iframe src="https://codesandbox.io/embed/61l8893lyw?fontsize=13&hidenavigation=1&module=%2Fstate%2Fcounter.js" style="width:100%; height:240px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>
+				</div>
+
+				<h2>To Do List:</h2>
+				<Code children={
+`
+const initial = () => ({
+	items: [
+		{ text: 'Make smoothie', done: false },
+		{ text: 'Dance in mirror', done: true }
+	]
+})
+
+const add = (props, text) => ({ items }) => ({
+	items: items.concat({ text, done: false })
+})
+
+const removeAtIndex = (props, index) => ({ items }) => ({
+	items: items.splice(index, 1)
+})
+
+const toggleAtIndex = (props, index) => ({ items }) => ({
+	items: items.map((item, currentIndex) =>
+		index === currentIndex ? { ...item, done: !item.done } : item
+	)
+})
+`
+				} />
 
 			</main>
 		);
