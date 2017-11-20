c30fef30ed4fc173d2543ab6c7dbebb61d99c242
Patrick Smith <patrick@burntcaramel.com>
2017-11-21 00:13:38 +1100

    Update nav urls to trailing slashes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,10 +9,10 @@ export default class Header extends Component {
 				<h1>»α«</h1>
 				<nav>
 					<Link activeClassName={style.active} href="/">Intro</Link>
-					<Link activeClassName={style.active} href="/handlers">Handlers</Link>
-					<Link activeClassName={style.active} href="/react">React</Link>
-					<Link activeClassName={style.active} href="/preact">Preact</Link>
-					<Link activeClassName={style.active} href="/redux">Redux</Link>
+					<Link activeClassName={style.active} href="/handlers/">Handlers</Link>
+					<Link activeClassName={style.active} href="/react/">React</Link>
+					<Link activeClassName={style.active} href="/preact/">Preact</Link>
+					<Link activeClassName={style.active} href="/redux/">Redux</Link>
 				</nav>
 			</header>
 		);
