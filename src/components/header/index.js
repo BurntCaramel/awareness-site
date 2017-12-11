5019f1be4edbe1e095705a95a14fab0dd6e61ddb
Patrick Smith <patrick@burntcaramel.com>
2017-12-11 16:03:55 +1100

    Improve primary nav

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,14 +5,11 @@ import style from './style';
 export default class Header extends Component {
 	render() {
 		return (
-			<header class={style.header}>
-				<h1>»α«</h1>
+			<header className={style.header}>
+				<h1>{'α'}</h1>
 				<nav>
-					<Link activeClassName={style.active} href="/">Intro</Link>
+					<Link activeClassName={style.active} href="/">Getting Started</Link>
 					<Link activeClassName={style.active} href="/handlers/">Handlers</Link>
-					<Link activeClassName={style.active} href="/react/">React</Link>
-					<Link activeClassName={style.active} href="/preact/">Preact</Link>
-					<Link activeClassName={style.active} href="/redux/">Redux</Link>
 				</nav>
 			</header>
 		);
