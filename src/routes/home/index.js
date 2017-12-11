c1c7ba1e66de6beddce2655ef2bae81b639ad185
Patrick Smith <patrick@burntcaramel.com>
2017-12-11 15:36:50 +1100

    Add installation instructions

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -8,13 +8,26 @@ export default class Home extends Component {
 			<main class={style.home}>
 				<h1>Awareness</h1>
 
-				<p>Bring your components to life with loaded data, animation, and multi-step actions.</p>
+				<p>Bring your components to life with loaded data, animation, and multi-step asynchronous actions.</p>
 
 				<p>Works with <strong>React</strong>, <strong>Preact</strong> & <strong>Redux</strong>.</p>
 
 				<p>Core is <a href="https://bundlephobia.com/result?p=awareness">less than 1 KB gzipped</a>.</p>
 
+				<h2>Installation</h2>
+				<ul>
+					<li><strong>React:</strong> <code>yarn add react-organism</code></li>
+					<li><strong>Preact:</strong> <code>yarn add preact-organism</code></li>
+					<li><strong>Redux:</strong> <code>yarn add redux-organism</code></li>
+				</ul>
+
 				<div>
+					<h2>Minesweeper clone</h2>
+					<iframe src="https://codesandbox.io/embed/github/RoyalIcing/minesweeter/tree/master/?fontsize=13&hidenavigation=1&module=%models%2Fgame.js" style="width:100%; height:740px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>
+				</div>
+
+				<div>
+					<h2>Counter</h2>
 					<iframe src="https://codesandbox.io/embed/61l8893lyw?fontsize=13&hidenavigation=1&module=%2Fstate%2Fcounter.js" style="width:100%; height:240px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>
 				</div>
 
